b96027f655bb65df1c615b1d57922db1973ce82a
zaldyprtm <zaldyprtm@gmail.com>
2024-01-16 11:05:08 +0700

    add flow form

---
+++ src/js/script.js
@@ -0,0 +1,27 @@
+
+    function openSignupModal() {
+      var modal = document.getElementById('signupModal');
+      var bdy = document.querySelector('body');
+      modal.style.display = 'flex';
+      bdy.style.backgroundColor = 'gray';
+    }
+
+    function closeSignupModal() {
+      var modal = document.getElementById('signupModal');
+      modal.style.display = 'none';
+    }
+
+    function signup() {
+      var username = document.getElementById('username').value;
+      var email = document.getElementById('email').value;
+      var password = document.getElementById('password').value;
+
+      // Lakukan validasi data atau kirim data ke server sesuai kebutuhan
+
+      // Contoh: Menampilkan alert dengan data yang diisi
+      alert('Username: ' + username + '\nEmail: ' + email + '\nPassword: ' + password);
+
+      // Tutup modal setelah sign up berhasil
+      var modal = document.getElementById('signupModal');
+      modal.style.display = 'none';
+    }
